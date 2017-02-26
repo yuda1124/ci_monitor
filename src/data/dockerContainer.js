8ed0d7f3cc4be4c4cad2993d108b2660a83808b9
Chan Kim <ovekyc@gmail.com>
2017-02-25 22:52:04 -0800

    Removed useless files

--- src/data/dockerContainer.js
+++
@@ -1,8 +0,0 @@
-function dockerContainer(name, id, status, created, ports, usage) {
-  this.name = name;
-  this.id = id;
-  this.status = status;
-  this.created = created;
-  this.ports = ports;
-  this.usage = usage;
-}
--- src/data/usage.js
+++
@@ -1,6 +0,0 @@
-function usage(cpu, mem, disk, network) {
-  this.cpu = cpu;
-  this.mem = mem;
-  this.disk = disk;
-  this.network = network;
-}
