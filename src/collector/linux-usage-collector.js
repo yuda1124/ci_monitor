72718ceff5b51b14c804f98df552cba90c83e19b
yuda1124 <yuda1124@naver.com>
2017-02-26 23:56:08 -0800

    added todo comment and implemented linux-usage-collector without network

--- src/collector/linux-usage-collector.js
+++ src/collector/linux-usage-collector.js
@@ -1,10 +1,30 @@
-// # this is a just copy to use it later.
-// function getCPU() {
-//   exec('top -bn2 | grep "Cpu(s)"', (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`exec error: ${error}`);
-//       return;
-//     }
-//     return stdout;
-//   });
-// }
+const exec = require('child_process').execSync;
+const Usage = require('../model/usage');
+
+function getCPUUsage() {
+  const stdout = exec('top -bn2 | grep "Cpu(s)"').toString(); // TODO: handling exception.
+  const stdoutArray = stdout.split('\n');
+  // we will use second value, because we detect that first value is always same.
+  const CPUInfo = stdoutArray[1];
+  const idIndex = CPUInfo.indexOf('id');
+  const idle =  CPUInfo.substring(idIndex-6, idIndex-1).trim();
+  const cpuUsage =  100 - idle;
+  return cpuUsage.toFixed(1);
+};
+
+function getMemoryUsage() {
+  const memTotal = exec('cat /proc/meminfo | grep "MemTotal"').toString().replace(/\D/g, ''); // TODO: handling exception.
+  const memFree = exec('cat /proc/meminfo | grep "MemFree"').toString().replace(/\D/g, ''); // TODO: handling exception.
+  const cpuUsage = (1 - (memFree / memTotal)) * 100;
+  return cpuUsage.toFixed(1);
+}
+
+function getDiskUsage() {
+  const stdout = exec('df -hl --total --output=source,pcent | grep "total"').toString(); // TODO: handling exception.
+  const diskUsage = stdout.replace(/\D/g, '');
+  return diskUsage;
+}
+
+function linuxUsageCollector() {
+
+}
